6c6d9f51033a42b1cc8326a302bc81a0c727867a
CCLancaster <connieclancaster@gmail.com>
2020-01-30 12:45:17 -0800

    made it work yay

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log("👀")
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log("👀");
+    
+
+    let getJoke = document.getElementById("getMeJoke");
+    getJoke.addEventListener("click", function(e) {
+    
+        console.log("Initiating fetch");
+        e.preventDefault();
+
+        // //fetch joke
+        // fetch('https://api.kanye.rest/')
+        //     .then((response) => {
+        //       return response.json();
+        //     })
+        //     .then((myJson) => {
+        //         console.log(myJson);
+        //         document.getElementById("joke").textContent = "butts";
+        //     });
+
+        fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                "Accept": "application/json", 
+            },
+        })
+            .then((response) => {
+                return response.json();
+            })
+           .then((myJson) => {
+                console.log(myJson);
+                document.getElementById("joke").textContent = `${myJson.joke}`;
+            });
+    });
+});
